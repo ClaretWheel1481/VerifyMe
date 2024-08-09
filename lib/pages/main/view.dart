bb054c259d3ed0c13050178bbc097a82f6b026fb
ClaretWheel1481 <245922575@qq.com>
2024-08-09 21:14:50 +0800

    init

---
+++ lib/pages/main/view.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class MainApp extends StatefulWidget {
+  const MainApp({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+    );
+  }
+}
